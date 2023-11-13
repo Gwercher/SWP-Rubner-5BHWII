cde2b4c5e4f85f59a0dc6adcc9ed9179c11152e2
Gwercher <sigwercher@tsn.at>
2023-11-13 15:26:20 +0100

    13/11/2023 02 Poker -> main()

--- 02_Poker/main.py
+++ 02_Poker/main.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def royal_flush(list):
+def royal_flush(list, statistics):
     royal_flush_values = [0, 9, 10, 11, 12]
     list.sort()
 
@@ -21,7 +21,7 @@ def royal_flush(list):
     return True
 
 
-def straight(list):
+def straight(list, statistics):
     mods = []
     for i in range(0, 5):
         mods.append(list[i] % 13)
@@ -34,7 +34,7 @@ def straight(list):
     return True
 
 
-def straight_flush(list):
+def straight_flush(list, statistics):
     list.sort()
     for i in range(1, 5):
         # check for same colors
@@ -49,7 +49,7 @@ def straight_flush(list):
     return True
 
 
-def flush(list):
+def flush(list, statistics):
     for i in range(1, 5):
         # check for same colors
         if list[i - 1] // 13 != list[i] // 13:
@@ -59,7 +59,7 @@ def flush(list):
     return True
 
 
-def four_of_a_kind(list):
+def four_of_a_kind(list, statistics):
     mods = []
     for i in range(1, 6):
         mods.append(list[i - 1] % 13)
@@ -72,7 +72,7 @@ def four_of_a_kind(list):
     return False
 
 
-def three_of_a_kind(list):
+def three_of_a_kind(list, statistics):
     mods = []
     for i in range(1, 6):
         mods.append(list[i - 1] % 13)
@@ -85,7 +85,7 @@ def three_of_a_kind(list):
     return False
 
 
-def pair(list):
+def pair(list, statistics):
     mods = []
     for i in range(1, 6):
         mods.append(list[i - 1] % 13)
@@ -98,7 +98,7 @@ def pair(list):
     return False
 
 
-def two_pair(list):
+def two_pair(list, statistics):
     mods = []
     for i in range(1, 6):
         mods.append(list[i - 1] % 13)
@@ -111,7 +111,7 @@ def two_pair(list):
     return False
 
 
-def full_house(list):
+def full_house(list, statistics):
     mods = []
     for i in range(1, 6):
         mods.append(list[i - 1] % 13)
@@ -124,64 +124,71 @@ def full_house(list):
     return False
 
 
-def high_card():
+def high_card(statistics):
     statistics['high_card'] += 1
 
 
-statistics = {
-    'royal_flush': 0,
-    'straight_flush': 0,
-    'four_of_a_kind': 0,
-    'full_house': 0,
-    'flush': 0,
-    'straight': 0,
-    'three_of_a_kind': 0,
-    'two_pair': 0,
-    'pair': 0,
-    'high_card': 0,
-}
+def main():
+    statistics = {
+        'royal_flush': 0,
+        'straight_flush': 0,
+        'four_of_a_kind': 0,
+        'full_house': 0,
+        'flush': 0,
+        'straight': 0,
+        'three_of_a_kind': 0,
+        'two_pair': 0,
+        'pair': 0,
+        'high_card': 0,
+    }
 
-cards_amount = 52
+    cards_amount = 52
 
-pull_cards_amount = 5
-tries = 1000000
+    pull_cards_amount = 5
+    tries = 100000
 
-for i in range(tries):
-    current_pair = random.sample(range(0, cards_amount), pull_cards_amount)
+    for i in range(tries):
+        current_pair = random.sample(range(0, cards_amount), pull_cards_amount)
 
-    if not royal_flush(current_pair):
-        if not straight_flush(current_pair):
-            if not four_of_a_kind(current_pair):
-                if not full_house(current_pair):
-                    if not flush(current_pair):
-                        if not straight(current_pair):
-                            if not three_of_a_kind(current_pair):
-                                if not two_pair(current_pair):
-                                    if not pair(current_pair):
-                                        high_card()
+        if not royal_flush(current_pair, statistics):
+            if not straight_flush(current_pair, statistics):
+                if not four_of_a_kind(current_pair, statistics):
+                    if not full_house(current_pair, statistics):
+                        if not flush(current_pair, statistics):
+                            if not straight(current_pair, statistics):
+                                if not three_of_a_kind(current_pair, statistics):
+                                    if not two_pair(current_pair, statistics):
+                                        if not pair(current_pair, statistics):
+                                            high_card(statistics)
 
-for i in statistics:
-    statistics[i] = (statistics[i] / tries) * 100
+    for i in statistics:
+        statistics[i] = (statistics[i] / tries) * 100
 
-# https://en.wikipedia.org/wiki/Poker_probability#5-card_poker_hands
-wikipedia_x = [0.000154, 0.00139, 0.02401, 0.1441, 0.1965, 0.3925, 2.1128, 4.7539, 42.2569, 50.1177]
+    # https://en.wikipedia.org/wiki/Poker_probability#5-card_poker_hands
+    wikipedia_x = [0.000154, 0.00139, 0.02401, 0.1441, 0.1965, 0.3925, 2.1128, 4.7539, 42.2569, 50.1177]
 
-print(f"Wikipedia %\t\t\t|\t\tGwercher %\t\t|\t\tCard Hand")
-print(f"--------------------|-----------------------|-----------------------")
+    print(f"Wikipedia %\t\t\t|\t\tGwercher %\t\t|\t\tCard Hand")
+    print(f"--------------------|-----------------------|-----------------------")
 
-j = 0
-for i in statistics:
-    print(f"{wikipedia_x[j]:.6f}\t\t\t|\t\t{statistics[i]:.6f}\t\t|\t\t{i}")
-    j += 1
-print(f"tries={tries}")
+    j = 0
+    for i in statistics:
+        print(f"{wikipedia_x[j]:.6f}\t\t\t|\t\t{statistics[i]:.6f}\t\t|\t\t{i}")
+        j += 1
+    print(f"tries={tries}")
 
-"""
-plt.bar(statistics.keys(), wikipedia_x, color="red")
-plt.xticks(rotation=45)
-plt.title("Wikipedia")
-plt.show()
+    """
+    plt.bar(statistics.keys(), wikipedia_x, color="red")
+    plt.xticks(rotation=45)
+    plt.title("Wikipedia")
+    plt.show()
+    
+    plt.bar(statistics.keys(), statistics.values())
+    plt.xticks(rotation=45)
+    plt.show()
+    """
 
-plt.bar(statistics.keys(), statistics.values())
-plt.xticks(rotation=45)
-plt.show()
-"""
+
+if __name__ == '__main__':
+    main()
+
+# TODO: unit testing, maybe change to oop
